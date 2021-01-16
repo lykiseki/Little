b9a4ab34ef209bd1a12dc764dc137e738e34fdaa
lykiseki <73599969+lykiseki@users.noreply.github.com>
2021-01-16 09:55:40 +0800

    Update jd_fruit.js

--- jd_fruit.js
+++ jd_fruit.js
@@ -39,7 +39,7 @@ let shareCodes = [ // 这个列表填入你要助力的好友的shareCode
     //ly-梦-羊
   '4fc147a47a2b45f2ac7e31c3e1315976@ed87892d286f4f7397b1317650dbec57@d28ca23ddb934143801ce74229927ac2',
    //账号三的好友shareCode,不同好友的shareCode中间用@符号隔开
-  //ly-梦-离-羊
+  //ly-梦
   '4fc147a47a2b45f2ac7e31c3e1315976@ed87892d286f4f7397b1317650dbec57',
 ]
 let message = '', subTitle = '', option = {}, isFruitFinished = false;
